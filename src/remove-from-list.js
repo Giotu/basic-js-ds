8105edaed219b8bfe50724e361d8f268b4ead4a1
Giotu <evilalex1998@mail.ru>
2023-12-22 22:01:26 +0300

    feat: add task remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+	if (!l) {
+		return;
+	}
+
+	// let head = l;
+	let list = l;
+	let prev = null;
+	while (list) {
+		if (list.value === k) {
+			if (list === l) {
+				l = l.next;
+			} else {
+				list = list.next;
+				prev.next = list;
+				continue;
+			}
+		}
+		prev = list;
+		if (!list.next) break;
+		list = list.next;
+	}
+	return l;
 }
 
 module.exports = {
-  removeKFromList
+	removeKFromList,
 };
