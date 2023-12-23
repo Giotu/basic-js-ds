6ce00d75b2e0d5fc6a9bc94ea1c16164eb736260
Giotu <evilalex1998@mail.ru>
2023-12-23 21:58:00 +0300

    feat: add method has

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -32,9 +32,8 @@ class BinarySearchTree {
 		}
 	}
 
-	has(/* data */) {
-		throw new NotImplementedError("Not implemented");
-		// remove line with error and write your code here
+	has(data) {
+		return Boolean(this.find(data));
 	}
 
 	find(data) {
