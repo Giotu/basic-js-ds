b46f18e6f98baf797f9182606717322a07eba4d0
Giotu <evilalex1998@mail.ru>
2023-12-23 21:56:09 +0300

    feat: add method find

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -37,9 +37,23 @@ class BinarySearchTree {
 		// remove line with error and write your code here
 	}
 
-	find(/* data */) {
-		throw new NotImplementedError("Not implemented");
-		// remove line with error and write your code here
+	find(data) {
+		let node = findNode(this.rootNode, data);
+
+		function findNode(node, data) {
+			if (!node) {
+				return null;
+			}
+			if (node.data === data) {
+				return node;
+			}
+			if (node.data > data) {
+				return findNode(node.left, data);
+			} else {
+				return findNode(node.right, data);
+			}
+		}
+		return node;
 	}
 
 	remove(/* data */) {
